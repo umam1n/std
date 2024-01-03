d9f1b46e059e02281af1017bd6a2060c65b5c86b
umam1n <151716633+umam1n@users.noreply.github.com>
2024-01-03 13:54:30 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,21 +1,182 @@
 #include <iostream>
-#include "tubes.h"
+#include "inventori.h"
 
 using namespace std;
 
-int main() {
+int main()
+{
     list_users LU;
-    list_tranksaksi LT;
     list_gudang LG;
     list_barang LB;
+    list_tranksaksi LT;
 
-    // Initialize lists
     createListUser(LU);
-    createListTranksaksi(LT);
     createListGudang(LG);
     createListBarang(LB);
+    createListTranksaksi(LT);
 
-    mainMenu(LU);
+    adrUser U;
+    adrGud G;
+    adrBar B;
+    adrTrank T;
+
+    //Deklarasi Variabel Data Pelanggan
+    infotypeUser DU;
+
+    //Deklarasi Variabel Data Barang
+    infotypeGud DG;
+    infotypeBar DB;
+
+    int pilihan = selectMenu();
+    while (pilihan != 0 ) {
+        switch(pilihan) {
+        case 1:
+            {
+            cout << "Tambahkan Data User" << endl;
+            cout << "Nama    : ";
+            cin >> DU.nama;
+            cout << "ID      : ";
+            cin >> DU.id;
+            cout << "Password: ";
+            cin >> DU.password;
+            insertUser(LU,DU);
+            }
+            break;
+        case 2:
+            {
+            cout <<"MENAMPILKAN SELURUH DATA USER"<<endl;
+            printUser(LU);
+            }
+            break;
+        case 3:
+            {
+            int idUser;
+            cout << "HAPUS DATA USER" <<endl;
+            cout << "ID : ";
+            cin >> idUser;
+            deleteUserWithTransactions(LU, LT, idUser);
+            }
+            break;
+        case 4:
+            {
+            string namaUser;
+            adrUser found;
+            cout << "Mencari Data User" <<endl;
+            cout <<"Masukkan Nama User: ";
+            cin >> namaUser;
+            found = searchUser(LU,namaUser);
+            if (found != nil) {
+                cout << endl <<"Data Yang Dicari: "<<endl;
+                cout << "Nama          : " << infoU(found).nama <<endl;
+                cout << "ID            : " << infoU(found).id<<endl;
+                cout << "Password      : "<<infoU(found).password<<endl;
+            } else {
+                cout << "===================================="<<endl;
+                cout << "|  DATA PELANGGAN TIDAK DITEMUKAN   |"<<endl;
+                cout << "===================================="<<endl;
+            }
+            }
+            break;
+        case 5:
+            {
+            cout << "Tambahkan Data Gudang" <<endl;
+            cout << "Nama Gudang       : ";
+            cin >> DG.nama;
+            cout << "Limit Gudang      : ";
+            cin >> DG.limit;
+            cout << "Terisi sekarang   : ";
+            cin >> DG.isi;
+            insertFirstGudang(LG,DG);
+            }
+            break;
+        case 6:
+            {
+            string namaGudang;
+            adrGud found;
+            cout << "Mencari Data Gudang" <<endl;
+            cout <<"Masukkan Nama Gudang: ";
+            cin >> namaGudang;
+            found = searchGudang(LG,namaGudang);
+            if (found != nil) {
+                cout << endl <<"Data Yang Dicari: "<<endl;
+                cout << "Nama Gudang         : " << infoG(found).nama <<endl;
+                cout << "Limit Gudang        : " << infoG(found).limit<<endl;
+                cout << "Terisi sekarang     : " <<infoG(found).isi<<endl;
+            } else {
+                cout << "===================================="<<endl;
+                cout << "|  DATA GUDANG TIDAK DITEMUKAN   |"<<endl;
+                cout << "===================================="<<endl;
+            }
+            }
+            break;
+        case 7:
+            {
+            string namaUser, namaGudang;
+            cout<< "Hubungkan User dengan Data Gudang"<<endl;
+            cout << "Nama User untuk Dihubungkan: ";
+            cin >>namaUser;
+            cout << "Nama Gudang                   : ";
+            cin >> namaGudang;
+            connectUserToGudang(LU, LG, namaUser, namaGudang);
+            }
+            break;
+        case 8:
+            {
+            string namaUser, namaGudang, tanggalTranksaksi, jenisTranksaksi;
+            int jumlahTranksaksi;
+            cout << "Buat Tranksaksi Baru" << endl;
+            cout << "Nama User : ";
+            cin >> namaUser;
+            cout << "Nama Gudang ";
+            cin >> namaGudang;
+            cout << "Tanggal Tranksaksi : ";
+            cin >> tanggalTranksaksi;
+            cout << "Jenis Tranksaksi : ";
+            cin >> jenisTranksaksi;
+            cout << "Jumlah Tranksaksi: ";
+            cin >> jumlahTranksaksi;
+            connectUserToGudangWithTranksaksi(LU,LT,LG,namaUser,namaGudang, tanggalTranksaksi, jenisTranksaksi, jumlahTranksaksi);
+            }
+            break;
+        case 9:
+            {
+                    printTranksaksi(LT, LU, LG);
+            }
+            break;
+        case 10:
+            {
+            string namaBarang, namaGudang;
+            cout << "Tambah Barang ke Gudang" << endl;
+            cout << "Nama Barang : ";
+            cin >> namaBarang;
+            cout << "Nama Gudang : ";
+            cin >> namaGudang;
+            addBarangToGudang(LG, namaBarang, namaGudang);
+            }
+        case 11:
+            {
+                // Implement your logic to search Gudang for a specific User
+            }
+            break;
+        case 12:
+            {
+                // Implement your logic to delete Gudang for a specific User
+            }
+            break;
+        case 13:
+            {
+                // Implement your logic to count the number of Barang from a specific User
+            }
+            break;
+
+
+        }
+
+        pilihan = selectMenu();
+        }
+    cout<<"|-------------------------------------------------|"<<endl;
+    cout<<"|=========ANDA TELAH KELUAR DARI PROGRAM==========|"<<endl;
+    cout<<"|-------------------------------------------------|"<<endl;
 
     return 0;
 }
